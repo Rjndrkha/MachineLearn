b2415a5424d0bbb6d597c5cc5f3d27241eebafba
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2021-06-14 21:16:13 +0700

    ADD

--- resources/views/convert/pdftoword.blade.php
+++ resources/views/convert/pdftoword.blade.php
@@ -40,9 +40,9 @@
                             <path d="M0 4C0 1.79086 1.79086 0 4 0H28C30.2091 0 32 1.79086 32 4V28C32 30.2091 30.2091 32 28 32H4C1.79086 32 0 30.2091 0 28V4Z" fill="#F23030"></path>
                             <path d="M7.68718 7.68718L15.8787 9.73507L16 9.76539L16.1213 9.73507L24.3128 7.68718L22.2649 15.8787L22.2346 16L22.2649 16.1213L24.3128 24.3128L16.1213 22.2649L16 22.2346L15.8787 22.2649L7.68718 24.3128L9.73507 16.1213L9.76539 16L9.73507 15.8787L7.68718 7.68718Z" stroke="white"></path>
                         </svg></div>
-                    <h1 class="sc-17y9jfw-2 lpnhkH">Convert Image To Text</h1>
+                    <h1 class="sc-17y9jfw-2 lpnhkH">Convert PDF to Word</h1>
                 </div>
-                <h2 class="sc-17y9jfw-3 lpvDOQ">Convert Your Image To Text</h2>
+                <h2 class="sc-17y9jfw-3 lpvDOQ">Convert Your PDF to Word</h2>
             </div>
         </div>
     </div>
